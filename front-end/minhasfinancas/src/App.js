dc3c821153e56bf7fd7f1e344fa30613ca956cbc
Wayster Melo <wayster.melo@citygusa.local>
2024-05-14 07:42:36 -0300

    card js

--- front-end/minhasfinancas/src/App.js
+++ front-end/minhasfinancas/src/App.js
@@ -1,35 +1,16 @@
 import React from 'react';
-import './App.css';
+import 'bootswatch/dist/flatly/bootstrap.css';
 
 class App extends React.Component {
 
-  state= { 
-    numero1:null,
-    numero2:null,
-    resultado: null
-  }
-
-  somar=()=>{
-      const resultado = parseInt(this.state.numero1) + parseInt(this.state.numero2);
-      this.setState({resultado: resultado});
-  }
 
   render() {
-      return (
-        <div>
-        <label>Primeiro numero:</label>
-        <input type = "text" value={this.state.numero1} onChange={(e) => this.setState({numero1: e.target.value})}/>
-              <br/>
-        <label>Segundo numero:</label>
-        <input type = "text" value={this.state.numero2} onChange={(e) => this.setState({numero2: e.target.value})}/>
-              <br/>
-    
-    
-    <button onClick={this.somar}>Somar</button>
-       <br/>
-        o resultado é: {this.state.resultado}
-      </div>
-      )
+         return (
+          <div>
+             <button type={'button'} className={'btn btn-primary'}>Primary</button>
+          </div>
+         )
+
   }
 }
 
